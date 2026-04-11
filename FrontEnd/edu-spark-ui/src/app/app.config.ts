ba477b5d66624faecda280e45763d95a98a70a9d
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-11 14:31:21 +0200

    chore: update app.config configuration

--- FrontEnd/edu-spark-ui/src/app/app.config.ts
+++ FrontEnd/edu-spark-ui/src/app/app.config.ts
@@ -3,11 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpRequestInterceptor } from './services/spinner-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(CarouselModule.forRoot()),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:HttpRequestInterceptor,
+      multi:true
+    }
   ]
 };
