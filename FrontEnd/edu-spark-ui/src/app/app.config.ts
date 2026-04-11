81fb18a0feba2441f4dd0b1e67779106782dddd4
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-11 14:35:29 +0200

    chore: update app.config configuration

--- FrontEnd/edu-spark-ui/src/app/app.config.ts
+++ FrontEnd/edu-spark-ui/src/app/app.config.ts
@@ -1,14 +1,38 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { RatingModule } from 'ngx-bootstrap/rating';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpRequestInterceptor } from './services/spinner-interceptor';
+//import { SafePipe } from './pipes/safe.pipe';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(CarouselModule.forRoot()),
+    importProvidersFrom(
+      CarouselModule.forRoot(),
+      AccordionModule.forRoot(),
+      RatingModule.forRoot(),
+      ModalModule.forRoot(),
+      PopoverModule.forRoot(),
+      //SafePipe,
+      BrowserModule,
+      BrowserAnimationsModule,
+      ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-top-right',
+        preventDuplicates: true,
+      }),
+      NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
+    ),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
