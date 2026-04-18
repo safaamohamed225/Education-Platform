3bb7cd1c81519fa23a5f054f92eb71b870d44668
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-18 18:20:10 +0200

    feat(api): implement ResponseBodyLoggingMiddleware to capture HTTP response bodies

---
+++ EduSpark.API/Middlewares/ResponseBodyLoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+
+namespace EduSpark.API.Middlewares
+{
+    public class ResponseBodyLoggingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var originalBodyStream = context.Response.Body;
+            try
+            {
+                using var memoryStream = new MemoryStream();
+                context.Response.Body = memoryStream;
+                await next(context);
+                memoryStream.Position = 0;
+                var reader = new StreamReader(memoryStream);
+                var responseBody = await reader.ReadToEndAsync();
+                memoryStream.Position = 0;
+                await memoryStream.CopyToAsync(originalBodyStream);
+                var requestTelemetry = context.Features.Get<RequestTelemetry>();
+                requestTelemetry?.Properties.Add("ResponseBody", responseBody);
+            }
+            finally
+            {
+                context.Response.Body = originalBodyStream;
+            }
+        }
+    }
+}
