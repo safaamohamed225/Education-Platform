49c3129fb52a0241da4fd0b60466a6493858b9bf
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-20 16:39:21 +0200

    Adding Entities to project

---
+++ EduSpark.Functions/EduSpark.Functions/Entities/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduSpark.Core.Entities;
+
+[Table("Review")]
+public partial class Review
+{
+    [Key]
+    public int ReviewId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int Rating { get; set; }
+
+    public string? Comments { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime ReviewDate { get; set; }
+
+    [ForeignKey("CourseId")]
+    [InverseProperty("Reviews")]
+    public virtual Course Course { get; set; } = null!;
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Reviews")]
+    public virtual UserProfile User { get; set; } = null!;
+}
