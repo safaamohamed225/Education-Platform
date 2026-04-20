c1dfb99288adcb2004e44108c805ecee14263f37
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-20 15:09:52 +0200

    Added profile Model

---
+++ EduSpark.Functions/EduSpark.Functions/Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿namespace EduSpark.Models
+{
+    public class Profile
+    {
+        public int UserId { get; set; }
+
+        public string DisplayName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string AdObjId { get; set; } = null!;
+        public List<string> Roles { get; set; } = null!;
+    }
+}
