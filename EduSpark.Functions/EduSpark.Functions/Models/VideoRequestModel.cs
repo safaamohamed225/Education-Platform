632377771e28aaa856458f92132e014fc009b057
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-20 18:12:40 +0200

    Added VideoRequestModel

---
+++ EduSpark.Functions/EduSpark.Functions/Models/VideoRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EduSpark.Functions.Models
+{
+    public class VideoRequestModel
+    {
+        [JsonPropertyName("videoRequestId")]
+        public int VideoRequestId { get; set; }
+    }
+}
