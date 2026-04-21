e40e544fe8a8e9e24e8a88d55b651e4d683ae2e2
safaamohamed225 <safaa.m.ibrahem@gmail.com>
2026-04-21 19:38:43 +0200

    Add UpdateUserProfileModel to support updating user profile data including bio and profile picture upload.

---
+++ EduSpark.API/Model/UpdateUserProfileModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EduSpark.API.Model
+{
+    public class UpdateUserProfileModel
+    {
+        public required int UserId { get; set; }
+        public string? Bio { get; set; }
+        public IFormFile? Picture { get; set; }
+    }
+
+}
