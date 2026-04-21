3e4882dc6f0de8015a403c57a3b7ccfc55fbbb12
safaamohamed225 <safaa.m.ibrahem@gmail.com>
2026-04-21 18:17:02 +0200

    feat(api): add AdminRole authorization attribute for role-based access control

---
+++ EduSpark.API/Common/AdminRoleAttribute.cs
@@ -0,0 +1,26 @@
+﻿using EduSpark.API.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace EduSpark.API.Common
+{
+
+    namespace EduSpark.API.Common
+    {
+        public class AdminRoleAttribute : Attribute, IAuthorizationFilter
+        {
+            public void OnAuthorization(AuthorizationFilterContext context)
+            {
+                var userClaims = context.HttpContext.RequestServices.GetService<IUserClaims>();
+                var userRoles = userClaims?.GetUserRoles();
+                if (userRoles == null || !userRoles.Contains("Admin"))
+                {
+                    // Return 403 Forbidden if the user does not have the Admin role
+                    context.Result = new ForbidResult();
+                }
+            }
+        }
+    }
+
+}
