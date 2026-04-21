ab37eae519ba975682a00bdc6255c8a0be708123
Safa Mohamed <safamhmd291@gmail.com>
2026-04-21 13:48:24 +0200

    feat(api): add AutoMapper profile for VideoRequest ↔ VideoRequestModel mapping

---
+++ EduSpark.API/Common/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EduSpark.Core.Entities;
+using EduSpark.Core.Models;
+
+namespace EduSpark.API.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<VideoRequest, VideoRequestModel>()
+             .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => $"{src.User.FirstName}, {src.User.LastName}"));
+
+            CreateMap<VideoRequestModel, VideoRequest>()
+                .ForMember(dest => dest.User, opt => opt.Ignore());
+        }
+    }
+}
